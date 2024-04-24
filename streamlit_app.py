f02389277eed8742910b783139ce19c7ad88bb7d
beescrig <147986350+beescrig@users.noreply.github.com>
2024-04-24 17:14:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,42 +20,34 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
-#añado multiselect
-
-ingredients_list=st.multiselect(
-    'Choose up to 5 ingredients:'
+#añado un multiselect
+ingredients_list= st.multiselect(
+    'Choose up to 5 ingredientes:'
     , my_dataframe
-)
+    , max_selections=5
+    )
 
-#los datos devueltos son listas
 #st.write(ingredients_list)
 #st.text(ingredients_list)
 
-# "if ingredients_list" significa "if ingredients_list is not null" 
-
 if ingredients_list:
-
-    #creo la variable "ingredients_string"
-
+    
     ingredients_string= ''
     
-    #loop for
     for fruit_chosen in ingredients_list:
-        ingredients_string+= fruit_chosen #+ añade fruit_chosen a lo que ya existe
-
-    
+        ingredients_string += fruit_chosen + ''
+        
     #st.write(ingredients_string)
     
-    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-            values ('""" + ingredients_string + """')"""
-
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+                values ('""" + ingredients_string + """','"""+name_on_order+ """')"""
+    
     #st.write(my_insert_stmt)
-    #añado el submit button
-    time_to_insert=st.button('Submit Order')
+    #st.stop()
+
+    time_to_insert= st.button('Submit Order')
+    
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered!', icon="✅")
-
-
-

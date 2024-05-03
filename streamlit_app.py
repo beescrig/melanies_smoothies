35447aecbe0e009d64bc9b08b9b80a18665e7aab
beescrig <147986350+beescrig@users.noreply.github.com>
2024-05-03 15:10:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
-import pandas as pd
 
 
 # Write directly to the app
@@ -21,10 +20,6 @@ session=cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
-pd_df=my_dataframe.to_pandas()
-#st.dataframe=pd_df
-#st.stop()
-
 ingredients_list= st.multiselect(
     'Choose up to 5 ingredientes:'
     , my_dataframe
@@ -37,10 +32,6 @@ if ingredients_list:
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ''
-        
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        
         st.subheader(fruit_chosen + 'Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         fv_df=st.dataframe(data=fruityvice_response.json(), use_container_width=True)
@@ -58,9 +49,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered!', icon="✅")
-
-
-
 
     
 
